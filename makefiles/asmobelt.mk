d713c6c5263e4e2fdd8fc6cb524b220f7a53d77e
Molkenov Askhat <askhat.molkenov@gmail.com>
2026-03-18 12:25:10 +0500

    update commands

--- makefiles/asmobelt.mk
+++ makefiles/asmobelt.mk
@@ -3,9 +3,35 @@ ASMOBELT_TARGET_DIR ?= .
 ASMOBELT_PROMPT_OUTPUT ?= prompt.txt
 
 .PHONY: add-path-comment concat-py-files
+.PHONY: asmobelt-format asmobelt-uuid
+.PHONY: docker-builder-prune docker-container-prune docker-image-prune docker-size docker-stop-all docker-system-prune
 
 add-path-comment:
 	asmobelt add-path-comment -d $(ASMOBELT_TARGET_DIR) || true
 
 concat-py-files:
 	asmobelt concat-py-files -p $(ASMOBELT_TARGET_DIR) -o $(ASMOBELT_PROMPT_OUTPUT) || true
+
+asmobelt-format:
+	asmobelt format
+
+asmobelt-uuid:
+	asmobelt uuid
+
+docker-builder-prune:
+	asmobelt docker-builder-prune
+
+docker-container-prune:
+	asmobelt docker-container-prune
+
+docker-image-prune:
+	asmobelt docker-image-prune
+
+docker-size:
+	asmobelt docker-size
+
+docker-stop-all:
+	asmobelt docker-stop-all
+
+docker-system-prune:
+	asmobelt docker-system-prune
